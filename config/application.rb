1e670d63b730ee416c236e8f58ed921e4a67c719
naoki-shi <31435915+naoki-shi@users.noreply.github.com>
2017-09-22 17:43:50 +0900

    apply jananese time

--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,7 @@ require_relative 'boot'
 
 require 'rails/all'
 
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -11,5 +12,9 @@ module WordCross
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+      
+      config.time_zone = 'Tokyo'
+      config.active_record.time_zone_aware_types = [:datetime, :time]
   end
 end
+
